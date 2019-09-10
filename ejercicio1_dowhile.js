9c69e29b557fba18718ef2bf66550f98d1888193
OctavioPalacios <ocpl2710@gmail.com>
2019-09-09 21:04:07 -0500

    Ejercicio1 DO-WHILE NODE JS

---
+++ ejercicio1_dowhile.js
@@ -0,0 +1,13 @@
+/********** DO-WHILE *************/ 
+let num1 = 0
+let num2 = 10
+let numActual = num1
+
+do{
+    console.log(numActual)   
+    numActual = numActual + 1
+}while(numActual <= num2)
+
+
+
+    
