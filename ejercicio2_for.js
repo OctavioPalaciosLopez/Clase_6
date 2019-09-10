e761c957f6b50a59af2be2de5739c1d6eb6a2c54
OctavioPalacios <ocpl2710@gmail.com>
2019-09-09 21:05:17 -0500

    Ejercicio2 FOR NODE JS

---
+++ ejercicio2_for.js
@@ -0,0 +1,12 @@
+/********** FOR *************/ 
+
+let pesoIdeal = 70
+let kilosXSemanas = 1
+let semanas = 0
+
+for(let peso = 80 ; peso > pesoIdeal; peso = peso - kilosXSemanas ){
+    semanas = semanas + 1 
+}
+console.log(semanas)
+
+    
