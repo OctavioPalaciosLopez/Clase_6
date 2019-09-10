0c179ccbeb6248287e755913b48bbf9ce9a23eda
OctavioPalacios <ocpl2710@gmail.com>
2019-09-09 21:05:02 -0500

    Ejercicio2 WHILE NODE JS

---
+++ ejercicio2_while.js
@@ -0,0 +1,13 @@
+/********** WHILE *************/ 
+let peso = 80 
+let pesoIdeal = 70
+let kilosXSemanas = 5
+let semanas = 0
+
+while(peso > pesoIdeal){
+    peso = peso - kilosXSemanas
+    semanas = semanas + 1 
+}
+console.log(semanas)
+
+    
