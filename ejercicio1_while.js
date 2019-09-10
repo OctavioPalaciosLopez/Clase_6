05d3d0e49d2654e19661c9faa77eb99b8e2fbd7f
OctavioPalacios <ocpl2710@gmail.com>
2019-09-09 21:03:45 -0500

    Ejercicio1 WHILE NODE JS

---
+++ ejercicio1_while.js
@@ -0,0 +1,11 @@
+/********** WHILE *************/ 
+let num1 = 0
+let num2 = 10
+let numActual = num1
+
+while(numActual <= num2){
+    console.log(numActual)   
+    numActual = numActual + 1
+}
+
+    
